87b939244418d8cf6d7f7f3c332f8a72fac985f9
Leizar06001 <rloussignian@icloud.com>
2024-02-02 18:08:29 +0100

    test

--- srcs/Server.cmd_names.cpp
+++ srcs/Server.cmd_names.cpp
@@ -7,9 +7,9 @@ void Server::cmd_names(int fd, vector<string> tokens){
 	// channels   = : public, * : secret, @ : private
 	// users      @ : operator
 	if (tokens.size() > 1){	// list users of a channel
-		msg = ":" + _servername + " " + toString(RPL_NAMREPLY) + " " + nick + " = #" + tokens[1] + " :" + _users->getListOfUsers();
+		msg = ":" + _servername + " " + toString(RPL_NAMREPLY) + " " + nick + " = " + tokens[1] + " :" + _users->getListOfUsers();
 		sendMessage(fd, msg);
-		msg = ":" + _servername + " " + toString(RPL_ENDOFNAMES) + " " + nick + " #" + tokens[1] + " :End of /NAMES list";
+		msg = ":" + _servername + " " + toString(RPL_ENDOFNAMES) + " " + nick + " " + tokens[1] + " :End of /NAMES list";
 		sendMessage(fd, msg);
 	} else {		// list all users
 		msg = ":" + _servername + " " + toString(RPL_NAMREPLY) + " " + nick + " = * :" + _users->getListOfUsers();
