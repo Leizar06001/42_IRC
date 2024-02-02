d3e4c374011de21126b6547141a7a1234a8b7b57
Leizar06001 <rloussignian@icloud.com>
2024-02-02 09:55:52 +0100

    again

--- srcs/Server.cmd_cap.cpp
+++ srcs/Server.cmd_cap.cpp
@@ -1,7 +1,7 @@
 #include "../includes/server.class.hpp"
 
 void Server::cmd_cap(int fd, vector<string> tokens){
-	if (!tokens.size() > 1){
+	if (tokens.size() > 1){
 		if (tokens[1] == "LS"){
 			sendMessage(fd, string("CAP * LS :"));
 		} else if (tokens[1] == "LIST"){
