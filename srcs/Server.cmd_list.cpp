f8fdea2c5bdf638426ad0b8edc24ed97ddff574d
Leizar06001 <rloussignian@icloud.com>
2024-02-05 19:50:50 +0100

    ;ist

--- srcs/Server.cmd_list.cpp
+++ srcs/Server.cmd_list.cpp
@@ -6,7 +6,7 @@ void	Server::cmd_list(int fd, vector<string>){
 	while (chan){
 		const string type = chan->channel_type;
 		if (type != "*"){
-			msg = type + " " + chan->channel_name + " " + toString(chan->nb_users) + " :" + chan->topic;
+			msg = chan->channel_name + " " + toString(chan->nb_users) + " :" + chan->topic;
 		}
 		sendServerMessage(fd, RPL_LIST, msg);
 		chan = _channels->getNextChannel(0);
