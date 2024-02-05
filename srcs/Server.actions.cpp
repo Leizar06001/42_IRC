fe5332fcc1e5ca732f892a5d13b58c7390c00423
Leizar06001 <rloussignian@icloud.com>
2024-02-05 03:22:50 +0100

    welcome

--- srcs/Server.actions.cpp
+++ srcs/Server.actions.cpp
@@ -19,11 +19,17 @@ void Server::performAction(userInfos* user){
 	int fd = user->getFd();
 
 	if (action_type == ACT_REGISTRATION){
-		string message = " :Welcome to the iRisChat network, " + user->getNickname() + "!" + user->getUsername() + "@" + user->getIpAdress();
-		sendMessage(fd, ":" + _servername + " 001 " + user->getNickname() + message);
-		//sendServerMessage(fd, RPL_WELCOME ,message);
+		// sendMessage(fd, ":" + _servername + " 001 " + user->getNickname() + " :Welcome to the iRisChat network, "
+		// 				+ user->getNickname() + "!" + user->getUsername() + "@" + user->getIpAdress());
 		// message = ":" + _servername + " 002 " + user->getNickname() + " :Your host is " + _servername + ", running version 0.1";
 		// sendMessage(fd, message);
+
+		sendServerMessage(fd, RPL_WELCOME, ":Welcome to the iRisChat network, " + user->getNickname() + "!" + user->getUsername() + "@" + user->getIpAdress());
+		sendServerMessage(fd, RPL_YOURHOST, ":Your host is " + _servername + ", running version 0.1");
+		sendServerMessage(fd, RPL_CREATED, ":This server was created at the beginning of the universe");
+		sendServerMessage(fd, RPL_MYINFO, _servername + " 0.1 iox ntb");
+		sendServerMessage(fd, RPL_ISUPPORT, "");
+
 		_users->validateRegistration(user);
 		vector<string> tokens;
 		tokens.push_back("JOIN");
