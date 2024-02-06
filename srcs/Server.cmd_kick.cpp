ad53bc744374aebb7ae055a351e91914cfdb980b
Leizar06001 <rloussignian@icloud.com>
2024-02-06 15:51:37 +0100

    kick

--- srcs/Server.cmd_kick.cpp
+++ srcs/Server.cmd_kick.cpp
@@ -13,8 +13,9 @@ void Server::cmd_kick(int fd, vector<string> tokens){
 		return ;
 	}
 
-	_channels->kickChannel(user, tokens[1].substr(1));
-	_term.prtTmColor(tokens[2] + " kicked of channel " + tokens[1], Terminal::BRIGHT_RED);
-
-	sendMsgToList(fd, "KICK " + tokens[1] + " " + tokens[2] + " :" + moder->getNickname(), _channels->getChannel(tokens[2])->users);
+	int ret = _channels->kickChannel(user, tokens[1]);
+	if (ret == 0){
+		_term.prtTmColor(tokens[2] + " kicked of channel " + tokens[1], Terminal::BRIGHT_RED);
+		sendMsgToList(fd, "KICK " + tokens[1] + " " + tokens[2] + " :" + moder->getNickname(), _channels->getChannel(tokens[2])->users);
+	}
 }
