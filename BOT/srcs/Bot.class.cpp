019665fbc72e73caaea98a25f140c7a9dca497c1
Leizar06001 <rloussignian@icloud.com>
2024-02-09 00:48:25 +0100

    Update Bot.class.cpp

--- BOT/srcs/Bot.class.cpp
+++ BOT/srcs/Bot.class.cpp
@@ -53,6 +53,9 @@ s_user* Bot::createNewUser(const string& nick){
 	user->nb_warnings_forbidden = 0;
 	user->last_warning_forbidden = 0;
 	user->nb_warnings_spam = 0;
+	user->nb_times_kicked = 0;
+	user->last_kick = 0;
+	user->banned = false;
 	prt(nick + " new user create\n", BRIGHT_CYAN);
 	return user;
 }
