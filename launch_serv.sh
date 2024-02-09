5e3d85cc2a6e3dbf412730da8caf7b90186dbb1e
Leizar06001 <rloussignian@icloud.com>
2024-02-09 20:12:19 +0100

    Update launch_serv.sh

--- launch_serv.sh
+++ launch_serv.sh
@@ -2,6 +2,8 @@
 counter=0
 while true; do
 	valgrind -s --leak-check=full --log-file="valgrind_${counter}.log" ./irc_server 6667 irc42
+	cp ./log/connections.log ./log/connections_${counter}.log
+	cp ./log/last_log.txt ./log/last_log_${counter}.txt
 	echo "Launching new server in 5 seconds..."
 	sleep 5
 	let counter++
